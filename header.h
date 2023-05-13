69dfb593858cae7db14c75eeaca8779be28b4c68
StrafeStopper <strafestopper@gmail.com>
2023-05-12 18:15:39 -0600

    made some changes

--- header.h
+++ header.h
@@ -1,1 +1,5 @@
 #include <stdio.h>
+#include <iostream>     // input-output stream
+#include <windows.h>    // importing windows function
+#include <conio.h>      // console input-output
+#include <fstream>      // file input-output stream
--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
 #include "header.h"
+
+using namespace std;
+
+int main( int argc, char* args[])
+{
+    printf("Welcome to simple-macro! \nThis is a simple command line tool that allows the user to run code, commands, long macros, etc with the push of a button. ");
+    return 0;
+}
